fca3656f9645df3365cbf69cdeac242eb7188236
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-07 23:32:39 +0630

    -

--- DotNet8Mvc.NLayerArchitecture.Shared/DevCode.cs
+++ DotNet8Mvc.NLayerArchitecture.Shared/DevCode.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8Mvc.NLayerArchitecture.Shared;
 
-namespace DotNet8Mvc.NLayerArchitecture.Shared
+public static class DevCode
 {
-    public static class DevCode
+    public static bool IsNullOrEmpty(this string str)
     {
-        public static bool IsNullOrEmpty(this string str)
-        {
-            return String.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-        }
+        return String.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
     }
 }
