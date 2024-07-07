363e4525e57629786a001a64f03078ffee560ec0
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-07 23:30:27 +0630

    Format Code

--- DotNet8Mvc.NLayerArchitecture.Models/Features/Blog/BlogModel.cs
+++ DotNet8Mvc.NLayerArchitecture.Models/Features/Blog/BlogModel.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8Mvc.NLayerArchitecture.Models.Features.Blog;
 
-namespace DotNet8Mvc.NLayerArchitecture.Models.Features.Blog
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
