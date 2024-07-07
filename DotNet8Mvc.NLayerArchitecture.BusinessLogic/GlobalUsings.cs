dca7a2a0400b690ba774876d58959a68e33b4912
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-07 23:25:44 +0630

    [Added] Global Usings in BL

---
+++ DotNet8Mvc.NLayerArchitecture.BusinessLogic/GlobalUsings.cs
@@ -0,0 +1,6 @@
+// Global using directives
+
+global using DotNet8Mvc.NLayerArchitecture.DataAccess.Features.Blog;
+global using DotNet8Mvc.NLayerArchitecture.Models.Features;
+global using DotNet8Mvc.NLayerArchitecture.Models.Features.Blog;
+global using DotNet8Mvc.NLayerArchitecture.Models.Resources;
