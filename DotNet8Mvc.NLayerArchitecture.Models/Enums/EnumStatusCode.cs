feb52d7b1e41c7cca710125b2c47c70231519495
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-07 23:29:56 +0630

    Format Code

--- DotNet8Mvc.NLayerArchitecture.Models/Enums/EnumStatusCode.cs
+++ DotNet8Mvc.NLayerArchitecture.Models/Enums/EnumStatusCode.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8Mvc.NLayerArchitecture.Models.Enums;
 
-namespace DotNet8Mvc.NLayerArchitecture.Models.Enums
+public enum EnumStatusCode
 {
-    public enum EnumStatusCode
-    {
-        None,
-        Success = 200,
-        Created = 201,
-        Accepted = 202,
-        BadRequest = 400,
-        NotFound = 404,
-        Conflict = 409,
-        Locked = 423,
-        InternalServerError = 500
-    }
+    None,
+    Success = 200,
+    Created = 201,
+    Accepted = 202,
+    BadRequest = 400,
+    NotFound = 404,
+    Conflict = 409,
+    Locked = 423,
+    InternalServerError = 500
 }
