ca485bf3651da003e1a025fecb1109e93a44044c
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-07 23:26:50 +0630

    [Added] Global Usings in DA

---
+++ DotNet8Mvc.NLayerArchitecture.DataAccess/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// Global using directives
+
+global using DotNet8Mvc.NLayerArchitecture.DbService.AppDbContexts;
+global using DotNet8Mvc.NLayerArchitecture.Mapper;
+global using DotNet8Mvc.NLayerArchitecture.Models.Enums;
+global using DotNet8Mvc.NLayerArchitecture.Models.Features;
+global using DotNet8Mvc.NLayerArchitecture.Models.Features.Blog;
+global using DotNet8Mvc.NLayerArchitecture.Models.Resources;
+global using DotNet8Mvc.NLayerArchitecture.Shared;
+global using Microsoft.EntityFrameworkCore;
