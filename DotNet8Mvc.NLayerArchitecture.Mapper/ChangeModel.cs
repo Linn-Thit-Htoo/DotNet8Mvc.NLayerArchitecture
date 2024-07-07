193fd5a0a0e48d42abdb7d7edcceef6db71beccd
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-07 23:28:55 +0630

    Format Code

--- DotNet8Mvc.NLayerArchitecture.Mapper/ChangeModel.cs
+++ DotNet8Mvc.NLayerArchitecture.Mapper/ChangeModel.cs
@@ -1,34 +1,28 @@
 ﻿using DotNet8Mvc.NLayerArchitecture.DbService.AppDbContexts;
 using DotNet8Mvc.NLayerArchitecture.Models.Features.Blog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet8Mvc.NLayerArchitecture.Mapper
+namespace DotNet8Mvc.NLayerArchitecture.Mapper;
+
+public static class ChangeModel
 {
-    public static class ChangeModel
+    public static BlogModel Map(this TblBlog dataModel)
     {
-        public static BlogModel Map(this TblBlog dataModel)
+        return new BlogModel
         {
-            return new BlogModel
-            {
-                BlogId = dataModel.BlogId,
-                BlogTitle = dataModel.BlogTitle,
-                BlogAuthor = dataModel.BlogAuthor,
-                BlogContent = dataModel.BlogContent
-            };
-        }
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent
+        };
+    }
 
-        public static TblBlog Map(this BlogRequestModel requestModel)
+    public static TblBlog Map(this BlogRequestModel requestModel)
+    {
+        return new TblBlog
         {
-            return new TblBlog
-            {
-                BlogTitle = requestModel.BlogTitle,
-                BlogAuthor = requestModel.BlogAuthor,
-                BlogContent = requestModel.BlogContent
-            };
-        }
+            BlogTitle = requestModel.BlogTitle,
+            BlogAuthor = requestModel.BlogAuthor,
+            BlogContent = requestModel.BlogContent
+        };
     }
 }
