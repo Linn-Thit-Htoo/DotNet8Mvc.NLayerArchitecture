569b583c47cdcc4dc145cbbedb14476c85d8b8ac
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-07 23:34:48 +0630

    [Complete] N Layer with MVC

---
+++ DotNet8Mvc.NLayerArchitecture.Presentation/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using DotNet8Mvc.NLayerArchitecture.BusinessLogic.Features.Blog;
+global using DotNet8Mvc.NLayerArchitecture.Models.Features.Blog;
+global using Microsoft.AspNetCore.Mvc;
